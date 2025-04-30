cc97847d730c0fc957abc29ae2352f1f915a1319
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-04-30 10:29:10 +0530

    added readme + update schema + adding room logic

---
+++ readme.md
@@ -0,0 +1,1 @@
+notion link for schema -https://gifted-sweatpants-97f.notion.site/ws-app-basic-1e5e138549cd80aa81b2ddc9074accb5
--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,16 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+interface User {
+  socket: WebSocket;
+  room: String;
+}
+
 let userCount = 0;
-let allSockets: WebSocket[] = [];
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-  userCount++;
-  console.log(`User connected. Total users: ${userCount}`);
-
-  // Add the new socket to the array will change later on
-  allSockets.push(socket);
-
-  socket.on("message", (event) => {
-    console.log(`Received message: ${event}`);
-    allSockets.forEach((s) => {
-      if (s !== socket && s.readyState === WebSocket.OPEN) {
-        s.send(`User ${userCount} says: ${event}`);
-      }
-    });
-  });
-
-  socket.on("close", () => {
-    userCount--;
-    console.log(`User disconnected. Total users: ${userCount}`);
-    // Remove the socket from the array when it disconnects
-    allSockets = allSockets.filter((s) => s !== socket);
+  socket.on("message", (message) => {
+    //{room: String, message: String}
   });
 });

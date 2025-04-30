c56537d3b395bfc3b01d758226376d2c3392fd30
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-04-30 10:55:52 +0530

    added room logic

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,35 @@ let allSockets: User[] = [];
 wss.on("connection", (socket) => {
   socket.on("message", (message) => {
     //{room: String, message: String}
+    //   {
+    //     "type":"join",
+    //     "payload":{
+    //     "roomID":"123"
+    //     }
+    // }
+
+
+  //   {
+  //     "type":"chat",
+  //     "payload":{
+  //     "message":"hi there"
+  //     }
+  // }
+
+    const parsedMessage = JSON.parse(message as unknown as string);
+    if (parsedMessage.type === "join") {
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomID,
+      });
+    }
+
+    if(parsedMessage.type === "chat"){
+      allSockets.forEach((user) => {
+        if(user.room === parsedMessage.payload.roomID){
+          user.socket.send(JSON.stringify(parsedMessage));
+        }
+      });
+    }
   });
 });

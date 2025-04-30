a992ff1f5278cbf7e97fb26091b4565931664960
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-04-30 11:04:26 +0530

    changed code little but + tested the code

--- src/index.ts
+++ src/index.ts
@@ -20,15 +20,15 @@ wss.on("connection", (socket) => {
     //     }
     // }
 
-
-  //   {
-  //     "type":"chat",
-  //     "payload":{
-  //     "message":"hi there"
-  //     }
-  // }
+    //   {
+    //     "type":"chat",
+    //     "payload":{
+    //     "message":"hi there"
+    //     }
+    // }
 
     const parsedMessage = JSON.parse(message as unknown as string);
+    console.log(parsedMessage);
     if (parsedMessage.type === "join") {
       allSockets.push({
         socket,
@@ -36,9 +36,12 @@ wss.on("connection", (socket) => {
       });
     }
 
-    if(parsedMessage.type === "chat"){
+    if (parsedMessage.type === "chat") {
+      const sender = allSockets.find((user) => user.socket === socket);
+      if (!sender) return;
+
       allSockets.forEach((user) => {
-        if(user.room === parsedMessage.payload.roomID){
+        if (user.room === sender.room) {
           user.socket.send(JSON.stringify(parsedMessage));
         }
       });
